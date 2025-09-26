11caf1eb677fc9760266d3f1f604ef538ec2f478
wad <wadleyrmoise@gmail.com>
2025-09-26 18:11:42 -0400

    Understand/Revise write_callback(), note that thegiven string is a CStyle String, finally, use libxml2 to parse the html before scrape

--- WEBSCRAPERCPP/main.cpp
+++ WEBSCRAPERCPP/main.cpp
@@ -2,8 +2,60 @@
 #include <curl/curl.h>
 using namespace std;
 
+class  Memory {
+public:
+    char *data;
+    size_t size;
+};
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+
+    size_t realsize = size * nmemb;
+    auto *newm = static_cast<Memory *>(userdata);
+    char *temp = new char[newm->size + realsize + 1];
+    memcpy(temp, newm->data, newm->size);
+    memcpy(temp + newm->size, ptr, realsize);
+
+    delete[] newm->data;
+    newm->data = temp;
+    newm->size += realsize;
+    newm->data[newm->size] = '\0';
+
+
+    return realsize;
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    CURL *curl;
+    curl = curl_easy_init();
+    bool valid;
+
+    if (curl)
+    {
+        Memory memory {};
+        memory.data = static_cast<char*> (malloc(1));
+        memory.size = 1;
+
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.scrapethissite.com/pages/simple/");
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "JENNIFER-LAWRENCE");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &memory);
+
+        valid = curl_easy_perform(curl);
+        if (valid != CURLE_OK)
+        {
+            cerr << "curl_easy_perform() failed: ";
+        }
+        curl_easy_cleanup(curl);
+        cout << memory.data << endl;
+        delete [] memory.data;
+        return 1;
+    }
+
+
 }
+
+
