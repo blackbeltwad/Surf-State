596fba82be8f90a2ca1a69fbd6882fae4bdf384d
wad <wadleyrmoise@gmail.com>
2025-11-27 17:46:18 -0500

    Getting the html into a string now works

--- WEBSCRAPERCPP/main.cpp
+++ WEBSCRAPERCPP/main.cpp
@@ -2,58 +2,36 @@
 #include <curl/curl.h>
 using namespace std;
 
-class  Memory {
-public:
-    char *data;
-    size_t size;
-};
+
 
 size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
-
+    static_cast<string*>(userdata)->append(ptr, size * nmemb);
     size_t realsize = size * nmemb;
-    auto *newm = static_cast<Memory *>(userdata);
-    char *temp = new char[newm->size + realsize + 1];
-    memcpy(temp, newm-> data, newm -> size);
-    memcpy(temp + newm->size, ptr, realsize);
-    temp[newm->size + realsize] = '\0';
-
-    delete [] newm -> data;
-    newm -> data = temp;
-    newm->size += realsize;
-    newm->data[newm->size] = '\0';
-
+    return realsize;
 }
 
 int main()
 {
-    CURL *curl;
-    curl = curl_easy_init();
-    bool valid;
+    CURL *curl = curl_easy_init();
 
-    if (curl)
-    {
 
-        Memory memory {};
-        memory.data = static_cast<char*> (malloc(1));
-        memory.size = 1;
 
+
+        string memory;
         curl_easy_setopt(curl, CURLOPT_URL, "https://www.scrapethissite.com/pages/simple/");
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "JENNIFER-LAWRENCE");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &memory);
+        CURLcode valid = curl_easy_perform(curl);
 
-        valid = curl_easy_perform(curl);
+
         if (valid != CURLE_OK)
         {
             cerr << "curl_easy_perform() failed: ";
         }
         curl_easy_cleanup(curl);
-        cout << memory.data << endl;
-        delete [] memory.data;
-        return 1;
-    }
+        cout << memory << "balls" << endl;
+
+    return 0;
 
 
 }
